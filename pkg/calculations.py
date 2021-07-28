d40dc938b3ed7e03698f9b4bd8559530c0e713eb
KwanHoo <49335804+KwanHoo@users.noreply.github.com>
2021-07-28 16:33:39 +0900

    docs : package
    package file

---
+++ pkg/calculations.py
@@ -0,0 +1,13 @@
+def add(l,r):
+    return l + r
+
+def mul(l,r):
+    return l * r
+
+def div(l,r):
+    return l / r
+
+if __name__ == "__main__":
+    print(add(1,2))
+    print(mul(3,10))
+    print(div(8, 2))
---
+++ pkg/prints.py
@@ -0,0 +1,11 @@
+def prt1():
+    print("Nice boy!")
+
+def prt2():
+    print("20400!! 2400!!")
+
+
+# 단위실행(독립적으로 파일 실행)
+if __name__ == "__main__":
+    prt1()
+    prt2()
