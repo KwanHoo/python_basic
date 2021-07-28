ee213cd0e3ffe2bd155cf42efbb7eef90dd768ff
KwanHoo <49335804+KwanHoo@users.noreply.github.com>
2021-07-28 16:06:14 +0900

    prac : do understanding data type
    1) tuple
    2) set
    3) dictionary

---
+++ section04-1.py
@@ -0,0 +1,32 @@
+# 데이터 타입
+
+v_str1 = "niceMan"
+v_bool = True
+v_str2 = "Goodboy"
+v_float = 10.3
+v_int = 7
+v_dict = {
+    "name"  : "Kim",
+    "age" : 25
+}
+v_list = [3, 5, 7]
+v_tuple = 3, 5, 7
+v_set = {7, 8, 9}
+
+print(type(v_tuple))
+print(type(v_set))
+print(type(v_dict))
+
+
+i1 = 39
+i2 = 939
+big_int1 = 31415920303124425151234143
+big_int2 = 77777777777777777777777777
+f1 = 1.234
+f2 = 3.1415
+f3 = .5
+f4 = 10.
+print (i1 * i2)
+result = f3 + i2
+
+print(result, type(result))
