4724a9afa27b501fe64731ba025c82922386abba
KwanHoo <49335804+KwanHoo@users.noreply.github.com>
2021-08-14 19:00:53 +0900

    feat: bug handling
    - 저장 경로란 관련 리셋되는 부분 버그
    - 저장 시에 발생하는 에러 try except으로 메시지 출력
    ex) 저장 경로에 C:/ 로하는 경우 권한문제로 저장이 안됨
    ex) 존재하지 않는 드라이브로 저장경로 설정하는경우 저장이 안됨

--- gui_project/5_apply_options.py
+++ gui_project/5_apply_options.py
@@ -35,6 +35,7 @@ def del_file():
 def brows_dest_path():
     folder_selected = filedialog.askdirectory() # 폴더 선택
     if folder_selected == '': #사용자가 취소를 누를 때
+        print('폴더 선택 취소')
         return
     # print(folder_selected)
     txt_dest_path.delete(0, END)  # 먼저 저장되어있는거 삭제
@@ -43,87 +44,91 @@ def brows_dest_path():
 ###########
 # 이미지 통합
 def merge_image():
-    ############# 옵션처리---------
-    # 가로넓이
-    img_width = cmb_width.get()
-    if img_width == "원본유지":
-        img_width = -1 # -1 일때는 원본 기준으로 통합
-    else:
-        img_width = int(img_width)
+
+    try:  # 예외처리, 버그(저장 문제)
+            
+        ############# 옵션처리---------
+        # 가로넓이
+        img_width = cmb_width.get()
+        if img_width == "원본유지":
+            img_width = -1 # -1 일때는 원본 기준으로 통합
+        else:
+            img_width = int(img_width)
+        
+        # 간격
+        img_space = cmb_space.get()
+        if img_space == "좁게":
+            img_space = 30
+        elif img_space == "보통":
+            img_space = 60
+        elif img_space == "넓게":
+            img_sapce = 90
+        else:  # "없음"
+            img_space = 0
+
+        # 포맷
+        img_format = cmb_format.get().lower() # 확장자 값 받아와서 소문자로 변경
+        ##########################----- 
+
+        #print(list_file.get(0, END)) # 모든 파일 목록을 가지고 오기
+        images = [Image.open(x) for x in list_file.get(0, END)]
+
+        # 가로넓이 옵션 --- 이미지 사이즈 리스트에 넣어서 하나씩 처리
+        image_sizes = [] #(width1, height1),(wi2, hi2),(wi3, hi3),...
+        if img_width > -1 :
+            # width 값 변경
+            image_sizes =[(int(img_width),int(img_width * x.size[1] / x.size[0])) for x in images]
+        else:
+            # 원본 사이즈 사용
+            image_sizes = [(x.size[0], x.size[1]) for x in  images]
+
+        # +) 계산식
+        # ex) (100 * 60)img가 있음, -> width를 80으로 줄이면 height 는?
+        # (원본 w) : (원본 h) = (변경 w) : (변경 h)
+        #     x    :    y     =    x'    :   y'
+        #  xy' = yx'
+        #  y' = x'y / x  = 80 x 60 / 100 = 48
+
+        # 우리 코드에 대입하면?
+        # x = width = size[0]
+        # y = height = size[1]
+        # x' = img_width # 변경값 해야함
+        # y' = x'y / x = img_width * size[1] / size[0]
+
+        widths, heigths = zip(*(image_sizes))
+
+        max_width, total_height = max(widths), sum(heigths) # 최대 넓이와, 전체 높이
+
+
+
+        # 스케치북
+
+        if img_space > 0:  # 이미지 간격 옵션 조정
+            total_height += (img_space * (len(images) -1))
+
+        result_img = Image.new("RGB", (max_width, total_height), (255,255,255)) # 배경 흰색
+        y_offset = 0   # x는 그대로 인데 y 좌표 기준으로 하나씩 늘려가면서 붙이는 작업을 위한 변수 / y 위치
     
-    # 간격
-    img_space = cmb_space.get()
-    if img_space == "좁게":
-        img_space = 30
-    elif img_space == "보통":
-        img_space = 60
-    elif img_space == "넓게":
-        img_sapce = 90
-    else:  # "없음"
-        img_space = 0
+        # 프로그레스 바 추가
+        for idx, img in enumerate(images): # enumerate 이용하여 index 반환
+            # width가 원본유지가 아닐때에는 이미지 크기 조정
+            if img_width > -1:
+                img = img.resize(image_sizes[idx])
 
-    # 포맷
-    img_format = cmb_format.get().lower() # 확장자 값 받아와서 소문자로 변경
-    ##########################----- 
+            result_img.paste(img, (0,y_offset))
+            y_offset += (img.size[1] + img_space)# height 값 + 사용자가 지정한 간격
 
-    #print(list_file.get(0, END)) # 모든 파일 목록을 가지고 오기
-    images = [Image.open(x) for x in list_file.get(0, END)]
+            progress = (idx + 1) / len(images) * 100 # 실제 퍼센트 정보를 계산
+            p_var.set(progress)
+            progress_bar.update()
 
-    # 가로넓이 옵션 --- 이미지 사이즈 리스트에 넣어서 하나씩 처리
-    image_sizes = [] #(width1, height1),(wi2, hi2),(wi3, hi3),...
-    if img_width > -1 :
-        # width 값 변경
-        image_sizes =[(int(img_width),int(img_width * x.size[1] / x.size[0])) for x in images]
-    else:
-        # 원본 사이즈 사용
-        image_sizes = [(x.size[0], x.size[1]) for x in  images]
-
-    # +) 계산식
-    # ex) (100 * 60)img가 있음, -> width를 80으로 줄이면 height 는?
-    # (원본 w) : (원본 h) = (변경 w) : (변경 h)
-    #     x    :    y     =    x'    :   y'
-    #  xy' = yx'
-    #  y' = x'y / x  = 80 x 60 / 100 = 48
-
-    # 우리 코드에 대입하면?
-    # x = width = size[0]
-    # y = height = size[1]
-    # x' = img_width # 변경값 해야함
-    # y' = x'y / x = img_width * size[1] / size[0]
-
-    widths, heigths = zip(*(image_sizes))
-
-    max_width, total_height = max(widths), sum(heigths) # 최대 넓이와, 전체 높이
-
-
-
-    # 스케치북
-
-    if img_space > 0:  # 이미지 간격 옵션 조정
-        total_height += (img_space * (len(images) -1))
-
-    result_img = Image.new("RGB", (max_width, total_height), (255,255,255)) # 배경 흰색
-    y_offset = 0   # x는 그대로 인데 y 좌표 기준으로 하나씩 늘려가면서 붙이는 작업을 위한 변수 / y 위치
- 
-    # 프로그레스 바 추가
-    for idx, img in enumerate(images): # enumerate 이용하여 index 반환
-        # width가 원본유지가 아닐때에는 이미지 크기 조정
-        if img_width > -1:
-            img = img.resize(image_sizes[idx])
-
-        result_img.paste(img, (0,y_offset))
-        y_offset += (img.size[1] + img_space)# height 값 + 사용자가 지정한 간격
-
-        progress = (idx + 1) / len(images) * 100 # 실제 퍼센트 정보를 계산
-        p_var.set(progress)
-        progress_bar.update()
-
-    # 포맷 옵션 처리
-    file_name = "merge_photo_R." + img_format
-    dest_path = os.path.join(txt_dest_path.get(), file_name)  # 저장경로 설정
-    result_img.save(dest_path)  # 이미지 저장
-    msgbox.showinfo("알림", "작업이 완료되었습니다.")
-
+        # 포맷 옵션 처리
+        file_name = "merge_photo_R." + img_format
+        dest_path = os.path.join(txt_dest_path.get(), file_name)  # 저장경로 설정
+        result_img.save(dest_path)  # 이미지 저장
+        msgbox.showinfo("알림", "작업이 완료되었습니다.")
+    except Exception as err: # 예외처리
+        msgbox.showerror("에러", err)
 
 # 시작
 def start():
