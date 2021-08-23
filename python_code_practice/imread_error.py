b6afbdff7aee5f399297fe0391d9b1ac71c1a54c
KwanHoo <49335804+KwanHoo@users.noreply.github.com>
2021-08-23 17:20:49 +0900

    docs: rename directory
    docs: rename directory
    
    code_prac -> python code practice

--- python_code_practice/imread_error.py
+++ python_code_practice/imread_error.py
@@ -2,7 +2,8 @@
 from enlighten_inference import EnlightenOnnxModel
 import cv2
 # imread() src.empty() 에러 : 경로(path)를 '\'를 '/'로 바꾸고 r''로 해서 불러오기 
-img14 = cv2.imread(r'D:/python_basic/python_basic/gui_project/image10.png')
+# r"C:\Users\블라블라"  : r을 앞에 써주게되면 로우 스트링임, 이스케이프 문자상관없이 상관없이 그대로 쓴다는거
+img14 = cv2.imread(r'D:/python_basic/python_basic/gui_project/image/image10.png')
 
 model = EnlightenOnnxModel()
 
