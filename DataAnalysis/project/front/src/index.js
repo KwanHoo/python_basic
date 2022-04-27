aec63b044733ac65198d77305e206f56c54fa1ef
KwanHoo <49335804+KwanHoo@users.noreply.github.com>
2022-04-28 02:52:11 +0900

    [REFACTOR] : app.js 라우팅 추가로 index.js 코드 수정

---
+++ DataAnalysis/project/front/src/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import ReactDOM from 'react-dom';
+// import './index.css';
+import App from './App';
+import Main from './components/main/Main';
+import Login from './components/login/Login';
+import Register from "./components/register/Register";
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App/>
+  </React.StrictMode>,
+  document.getElementById("root")
+);
